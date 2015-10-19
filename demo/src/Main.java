0ff3ca66a6b519de9cc1925a2c07716820ec48fc
Fujio Harou <harouf@outlook.com>
2015-10-20 01:07:01 +0900

    update demo code

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
                 .thinQ("11001", "0c82a54f22f775a3ed8b97b2dea74036")
                 // wrap and build the library
                 .buildLibrary();
-        String result = library.call("19193365890", "19192389898"); //return value is call sid if success, otherwise error message.
+        String result = library.call("19193365890", "19192334784"); //return value is call sid if success, otherwise error message.
         System.out.println("result: " + result);
     }
 }
